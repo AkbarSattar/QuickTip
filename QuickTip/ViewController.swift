be24ec8c23d3f509c088dff62b4a9efaa1b766b9
Your Name Here <you@yourdomain.com>
2017-04-07 11:44:53 -0400

    Initial Commit

---
+++ QuickTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickTip
+//
+//  Created by Akbar Sattar on 4/7/17.
+//  Copyright © 2017 Akbar Sattar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
